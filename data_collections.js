c318ffe1815e1a7b1d73060cd1761c5c41f9b377
biso <bopoku89@gmail.com>
2024-05-28 01:38:04 -0400

    Completed part 3

--- data_collections.js
+++ data_collections.js
@@ -4,10 +4,10 @@ const csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,
 
 // Declare a variable that stores the number of columns in each row of data within the CSV. Instead of hard-coding four columns per row, expand your code to accept any number of columns. This should be calculated dynamically based on the first row of data.
 let rows = csv.split("\n")
-let headers = rows[0]
+let headers = rows[0].toLowerCase()
 let columns = headers.split(",") // Each piece of data separated by a comma is a column
 let columnCount = columns.length
-console.log(columnCount) 
+// console.log(columnCount) 
 
 // Store your results in a two-dimensional array.
 // Each row should be its own array, with individual entries for each column.
@@ -17,10 +17,24 @@ let rowData = []
 for (let row in rows) {
     rowData[rowData.length] = rows[row].split(",")
 }
-console.log(rowData)
+// console.log(rowData)
 
+// For each row of data in the result array produced by your code above, create an object where the key of each value is the heading for that value’s column.
+// Convert these keys to all lowercase letters for consistency.
+// Store these objects in an array, in the order that they were originally listed.
+// Since the heading for each column will be stored in the object keys, you do not need to create an object for the heading row itself.
+let newRowData = []
 
-
+for (let i=1; i<rows.length; i++) {
+    let values = rows[i].split(",")
+    let newObj = {}
+    
+    for (let j=0; j<values.length; j++){
+        newObj[columns[j]] = values[j]
+    }
+    newRowData.push(newObj)
+}
+console.log(newRowData)
 
 
 let cell1 = ""
