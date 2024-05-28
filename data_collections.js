317a47fd32e80a5d75521db55f95e080a507cd3f
biso <bopoku89@gmail.com>
2024-05-28 03:19:53 -0400

    Added part 4

--- data_collections.js
+++ data_collections.js
@@ -2,6 +2,8 @@ const csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,
 
 // const csv = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
 
+
+// PART ONE
 // Declare a variable that stores the number of columns in each row of data within the CSV. Instead of hard-coding four columns per row, expand your code to accept any number of columns. This should be calculated dynamically based on the first row of data.
 let rows = csv.split("\n")
 let headers = rows[0].toLowerCase()
@@ -9,6 +11,7 @@ let columns = headers.split(",") // Each piece of data separated by a comma is a
 let columnCount = columns.length
 // console.log(columnCount) 
 
+// PART TWO
 // Store your results in a two-dimensional array.
 // Each row should be its own array, with individual entries for each column.
 // Each row should be stored in a parent array, with the heading row located at index 0.
@@ -19,12 +22,12 @@ for (let row in rows) {
 }
 // console.log(rowData)
 
+// PART THREE
 // For each row of data in the result array produced by your code above, create an object where the key of each value is the heading for that value’s column.
 // Convert these keys to all lowercase letters for consistency.
 // Store these objects in an array, in the order that they were originally listed.
 // Since the heading for each column will be stored in the object keys, you do not need to create an object for the heading row itself.
 let newRowData = []
-
 for (let i=1; i<rows.length; i++) {
     let values = rows[i].split(",")
     let newObj = {}
@@ -34,7 +37,28 @@ for (let i=1; i<rows.length; i++) {
     }
     newRowData.push(newObj)
 }
+// console.log(newRowData)
+
+// PART FOUR
+// Remove the last element from the sorted array.
+lastElement = newRowData.pop()
+
+// Insert the following object at index 1: { id: "48", name: "Barry", occupation: "Runner", age: "25" }
+newRowData.splice(1, 0, {id: "48", name: "Barry", occupation: "Runner", age: "25" })
+
+// Add the following object to the end of the array:{ id: "7", name: "Bilbo", occupation: "None", age: "111" }
+newRowData.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" })
 console.log(newRowData)
+
+// Finally, use the values of each object within the array and the array’s length property to calculate the average age of the group. This calculation should be accomplished using a loop.
+let ages = 0
+let avgAge
+for (let i=0; i<newRowData.length; i++) {
+    let age = Number(newRowData[i].age)
+    ages += age
+    avgAge = ages / newRowData.length
+    
+} console.log(avgAge)
 
 
 let cell1 = ""
